dd70eb4bb333ab0d2fe0acfcbfbbd1265933d1d4
Frank Li <frank.x.li@gmail.com>
2023-06-17 20:09:33 -0400

    Updated sample projects

--- gull-admin/src/fake-db/mock.js
+++
@@ -1,8 +0,0 @@
-// const MockAdapter = require("axios-mock-adapter");
-// const axios = require("axios");
-// const Mock = new MockAdapter(axios);
-// export default Mock;
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
-const Mock = new MockAdapter(axios);
-export default Mock;
