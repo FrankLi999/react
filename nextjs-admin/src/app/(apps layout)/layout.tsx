58435a01ffb76563705c6c79608fdfce4e6a739e
Frank Li <frank.x.li@gmail.com>
2023-12-21 23:29:57 -0500

    Updated next-auth config

--- nextjs-admin/src/app/(apps layout)/layout.tsx
+++ nextjs-admin/src/app/(apps layout)/layout.tsx
@@ -5,14 +5,15 @@ import { permanentRedirect, useRouter } from 'next/navigation'
 import { useSession } from "next-auth/react"
 
 const AppsLayout = ({ children }) => {
+    // client side only
     // const router = useRouter();
     const {data: session, status} = useSession();
     console.log(">>>>>>>AppsLayout...status, ", status);
     console.log(">>>>>>>AppsLayout...session, ", session);
-    if (status === "unauthenticated" || session?.token_provider === 'anonymous') {
-        // router.push('/auth/login');
-        permanentRedirect('/auth/signin');
-    }
+    // if (status === "unauthenticated" || session?.token_provider === 'anonymous') {
+    //     // router.push('/auth/login');
+    //     permanentRedirect('/auth/signin');
+    // }
     console.log(">>>>>>>AppsLayout...after redirect, ");
     return (
           <ProtectedSiteLayout>
