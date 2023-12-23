6c74fbcbd3d11902a54d2aeaa5cafd45e4d14f03
Frank Li <frank.x.li@gmail.com>
2023-12-22 23:45:36 -0500

    Update route.ts

--- nextjs-admin/src/app/api/integrator/configurations/route.ts
+++ nextjs-admin/src/app/api/integrator/configurations/route.ts
@@ -1,62 +1,69 @@
-import { getServerSession } from "next-auth";
+import apiRequest  from '@/utils/ApiRequest';
+
+const contextPath = `/api/configurations`;
 
 export async function GET() {
-  console.log('>>>>>>>>>>>protected ...');
-  const session = await getServerSession();
-  console.log('>>>>>>>>>>>protected .session..', session);
-  if (!session) {
-    return new Response("Unauthorized", {
-      status: 401,
-    });
-  }
 
-  return new Response("Hello, Next.js!", {
-    status: 200,
+  const apiResponse = await apiRequest({
+    method: 'GET',
+    url: contextPath,
+    headers: {
+      Accept: 'application/json'
+    },
   });
+
+  console.log(
+    `GET ${contextPath} result is: `,
+    JSON.stringify(apiResponse.json()),
+  );
+
+  return new Response(apiResponse.json(), {status: apiResponse.status()});
 }
 
+export async function POST(request: Request) {
+  const apiResponse = await apiRequest({
+    method: 'POST',
+    url: contextPath,
+    headers: { 'Content-Type': 'application/json', 'Accept': '*/*' },
+    body: request.body
+  });
 
-export async function POST() {
-    const res = await fetch('https://data.mongodb-api.com/...', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'API-Key': process.env.DATA_API_KEY!,
-      },
-      body: JSON.stringify({ time: new Date().toISOString() }),
-    })
-   
-    const data = await res.json()
-   
-    return Response.json(data)
-  }
+  console.log(
+    `POST ${contextPath} result is: `,
+    JSON.stringify(apiResponse.json()),
+  );
 
-  export async function PUT() {
-    const res = await fetch('https://data.mongodb-api.com/...', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'API-Key': process.env.DATA_API_KEY!,
-      },
-      body: JSON.stringify({ time: new Date().toISOString() }),
-    })
-   
-    const data = await res.json()
-   
-    return Response.json(data)
-  }
+  return new Response(apiResponse.json(), {status: apiResponse.status()});
+}
 
-  export async function DELETE() {
-    const res = await fetch('https://data.mongodb-api.com/...', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'API-Key': process.env.DATA_API_KEY!,
-      },
-      body: JSON.stringify({ time: new Date().toISOString() }),
-    })
-   
-    const data = await res.json()
-   
-    return Response.json(data)
-  }
+export async function PUT(request: Request) {
+  const apiResponse = await apiRequest({
+    method: 'PUT',
+    url: contextPath,
+    headers: { 'Content-Type': 'application/json', 'Accept': '*/*' },
+    body: request.body
+  });
+
+  console.log(
+    `POST ${contextPath} result is: `,
+    JSON.stringify(apiResponse.json()),
+  );
+
+  return new Response(apiResponse.json(), {status: apiResponse.status()});
+}
+
+export async function DELETE(request: Request) {
+  const apiResponse = await apiRequest({
+    method: 'DELETE',
+    url: contextPath,
+    headers: { 'Content-Type': 'application/json', 'Accept': '*/*' },
+    body: request.body
+  });
+
+  console.log(
+    `POST ${contextPath} result is: `,
+    JSON.stringify(apiResponse.json()),
+  );
+
+  return new Response(apiResponse.json(), {status: apiResponse.status()});
+}
