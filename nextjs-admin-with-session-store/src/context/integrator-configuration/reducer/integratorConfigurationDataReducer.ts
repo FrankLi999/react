a76b031b58228d76ca5132ed99ddcb5a998cd178
Frank Li <frank.x.li@gmail.com>
2023-12-29 14:21:16 -0500

    Update integratorConfigurationDataReducer.ts

--- nextjs-admin-with-session-store/src/context/integrator-configuration/reducer/integratorConfigurationDataReducer.ts
+++ nextjs-admin-with-session-store/src/context/integrator-configuration/reducer/integratorConfigurationDataReducer.ts
@@ -1,23 +1,37 @@
 // import { ConfigurationModel } from '@/type/ConfigurationModel';
+
+import { ConfigurationModel } from "@/type/ConfigurationModel";
+
 // ConfigurationModel[]
-export const integratorConfigurationDataInitialStates = {
-    configurations: [],
-    currentRow: {}
+export interface ConfigurationDataState {
+    configurations: ConfigurationModel [];
+    currentRow: ConfigurationModel | null;
 }
 
-export const integratorConfigurationDataReducer = (state = integratorConfigurationDataInitialStates, action) => {
+export interface ConfigurationDataAction {
+    type: string;
+    configurations?: ConfigurationModel[];
+    currentRow?: ConfigurationModel | null;
+
+}
+export const integratorConfigurationDataInitialStates = {
+    configurations: [],
+    currentRow: null
+}
+
+export const integratorConfigurationDataReducer = (state: ConfigurationDataState = integratorConfigurationDataInitialStates, action: ConfigurationDataAction): ConfigurationDataState => {
     switch (action.type) {
         case "set_configurations": {
             return {
                 ...state,
-                configurations: action.configurations,
-                currentRow: {}
+                configurations: action.configurations as ConfigurationModel[],
+                currentRow: null
             };
         };
         case "set_current_row": {
             return {
                 ...state,
-                currentRow: action.currentRow
+                currentRow: action.currentRow as ConfigurationModel
             };
         };
         default:
