1601bb2a4db6086851d8b6036ddac788dbf5b85f
Frank Li <frank.x.li@gmail.com>
2023-12-29 13:43:15 -0500

    Update route.ts

--- nextjs-admin-with-session-store/src/app/api/integrator/imports/route.ts
+++ nextjs-admin-with-session-store/src/app/api/integrator/imports/route.ts
@@ -1,9 +1,6 @@
-import apiRrequest  from '@/utils/ApiRequest';
-import multiparty from "multiparty";
-import { NextApiRequest, NextApiResponse } from 'next';
 import * as configurationRepo from '@/repo/ConfigurationRepo';
 import { ConfigData } from '@/dto/ConfigData';
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(request: Request) {
   // const contextPath = `/api/imports`;
   // const apiResponse = apiRequest({
   //   method: 'POST',
@@ -12,16 +9,24 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
   //   data: request.body
   // });
   try {
-    const form = new multiparty.Form();
-    const data = await new Promise((resolve, reject) => {
-      form.parse(req, function (err, fields, files) {
-        if (err) reject({ err });
-        resolve({ fields, files });
-      });
-    }) as ConfigData [];
-    console.log(`import data: `, JSON.stringify(data));
-    await configurationRepo.deleteProfiles(configData);
-    await configurationRepo.saveAll(configData);
+    // const form = new multiparty.Form();
+    // const configData = await new Promise((resolve, reject) => {
+    //   form.parse(req, function (err, fields, files) {
+    //     if (err) reject({ err });
+    //     resolve({ fields, files });
+    //   });
+    // }) as ConfigData [];
+     console.log(`import data: 1`);
+    const formData = await request.formData();
+    console.log(`import data: 2`);
+    const dataFile = formData.get('file') as File;
+    console.log(`import data: 3`, dataFile);
+    const dataconfigJsonString = Buffer.from(await dataFile.arrayBuffer()).toString();
+    
+    console.log(`import data: `, dataconfigJsonString);
+    const configData = JSON.parse(dataconfigJsonString) as ConfigData[];
+    // await configurationRepo.deleteProfiles(configData);
+    // await configurationRepo.saveAll(configData);
     // thenMany(findAll());
   
     const responseBody =  await configurationRepo.findAll();
@@ -30,9 +35,9 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
       responseBody,
     );
 
-    return res.status(200).json(responseBody);
-  } catch (err) {
-    return res.status(500).json(err);
+    return new Response(JSON.stringify(responseBody), {status: 200});
+  } catch (err: any) {
+    return new Response(err, {status: 500});
   }
   // return new Response(responseBody, {status: apiResponse.status});
 }
