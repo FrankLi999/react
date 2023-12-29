e987d580872ce422a5f0833bd9f0a09029a3d742
Frank Li <frank.x.li@gmail.com>
2023-12-29 13:45:24 -0500

    Update Account.tsx

--- nextjs-admin-with-session-store/src/components/account/Account.tsx
+++ nextjs-admin-with-session-store/src/components/account/Account.tsx
@@ -1,14 +1,13 @@
 import {signIn, signOut, useSession} from "next-auth/react"
-import styles from "./Account.module.css"
 import SessionPoke from "@/components/account/SessionPoke";
 import * as React from "react";
 
 export default function Account() {
-    const {data: session, status} = useSession();
+    const {data} = useSession();
     const sign_in_element = <button onClick={() => signIn("github")}>Sign in</button>;
     const sign_out_element = <button onClick={() => signOut()}>Sign out</button>;
-    const sign_element = ((status === "authenticated") && session?.token_provider !== 'anonymous') ? sign_out_element : sign_in_element;
-    console.log(">>>>>>>>> account, ", session);
+    const sign_element = (data && data.user?.name && data?.user?.name.startsWith('camel_anon_')) ? sign_in_element : sign_out_element;
+    console.log(">>>>>>>>> account, ", data);
     return (
         <>
             <SessionPoke></SessionPoke>
