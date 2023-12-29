7a3d37e11fc0fd47cff788786c715821439ae0eb
Frank Li <frank.x.li@gmail.com>
2023-12-29 13:42:13 -0500

    Update route.ts

--- nextjs-admin-with-session-store/src/app/api/auth/[...nextauth]/route.ts
+++ nextjs-admin-with-session-store/src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 // https://github.com/quanhua92/next-auth-ioredis-adapter-example
-import NextAuth, {Account, Profile, AuthOptions,  SessionStrategy, Awaitable, Session, User } from "next-auth"
-import { ObjectId } from "mongodb"
-import { cookies } from "next/headers";
+import NextAuth, {Account, Profile, AuthOptions, Session, User } from "next-auth"
+import * as headers from "next/headers";
 import Okta from "next-auth/providers/okta";
 import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials"
@@ -22,7 +21,6 @@ const session = {
     maxAge: 30 * 24 * 60 * 60, // 30 days
     updateAge: 24 * 60 * 60, // 24 hours
   }
-  
 interface RouteHandlerContext {
   params: { nextauth: string[] }
 }
@@ -37,10 +35,10 @@ export const authOptions = (request: NextRequest, context: RouteHandlerContext):
     request.method === "POST";
   const isGetCallback =
     params?.nextauth?.includes("callback") &&
-    request.method === "GET";  
+    request.method === "GET";
   const isCallback =
     params?.nextauth?.includes("callback") &&
-    request.method === "POST";    
+    request.method === "POST";
   console.log(">>>>>>>>>>>>authOptions>>>isCredentialsCallback>>", isCredentialsCallback);
   console.log(">>>>>>>>>>>>authOptions>>>isGetCallback>>", isGetCallback);
   console.log(">>>>>>>>>>>>authOptions>>>isCallback>>", isCallback);
@@ -54,13 +52,13 @@ export const authOptions = (request: NextRequest, context: RouteHandlerContext):
           },
       }),
       Okta({
-        clientId: process.env.OKTA_OAUTH2_CLIENT_ID as string,
-        clientSecret: process.env.OKTA_OAUTH2_CLIENT_SECRET as string,
-        issuer: process.env.OKTA_OAUTH2_ISSUER as string,
+        clientId: process.env['OKTA_OAUTH2_CLIENT_ID'] as string,
+        clientSecret: process.env['OKTA_OAUTH2_CLIENT_SECRET'] as string,
+        issuer: process.env['OKTA_OAUTH2_ISSUER'] as string,
       }),
       GitHubProvider({
-        clientId: process.env.AUTH_GITHUB_ID as string,
-        clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+        clientId: process.env['AUTH_GITHUB_ID'] as string,
+        clientSecret: process.env['AUTH_GITHUB_SECRET'] as string,
       }),
     ],
     callbacks: {
@@ -68,16 +66,16 @@ export const authOptions = (request: NextRequest, context: RouteHandlerContext):
         logger.info('winston >>> user signin ....' + user);
         log.info('log4js >>> user signin ....' + user);
         console.log(">>>>>>>>>sign in user>", user);
-        
+
         // const isCredentialsCallback =
         //   req.query?.nextauth?.includes("callback") &&
         //   req.query?.nextauth?.includes("credentials") &&
         //   req.method === "POST";
-        console.log(">>>>>>>>>sign in req query param>", params);  
-        console.log(">>>>>>>>>sign in req isCredentialsCallback>", isCredentialsCallback);  
-        console.log(">>>>>>>>>sign in req isGetCallback>", isGetCallback); 
+        console.log(">>>>>>>>>sign in req query param>", params);
+        console.log(">>>>>>>>>sign in req isCredentialsCallback>", isCredentialsCallback);
+        console.log(">>>>>>>>>sign in req isGetCallback>", isGetCallback);
         console.log(">>>>>>>>>>>>authOptions>>>isCallback>>", isCallback);
-        if (isCredentialsCallback) {       
+        if (isCredentialsCallback) {
           const sessionToken = randomUUID()
           const sessionExpiry = new Date(Date.now() + session.maxAge * 1000)
           const userSession = {
@@ -90,36 +88,36 @@ export const authOptions = (request: NextRequest, context: RouteHandlerContext):
             expires: sessionExpiry,
             // userAgent: req.headers["user-agent"] ?? null,
           };
-          console.log(">>>>>>>>> will create user session>>>>", userSession); 
-          const s = await adapter.createSession(userSession);
-          console.log(">>>>>>>>> user session created>>>>", s); 
-          cookies().set("next-auth.session-token", sessionToken, {
+          console.log(">>>>>>>>> will create user session>>>>", userSession);
+          adapter.createSession && await adapter.createSession(userSession);
+          // console.log(">>>>>>>>> user session created>>>>", s);
+          headers.cookies().set("next-auth.session-token", sessionToken, {
               expires: sessionExpiry,
           });
-          console.log(">>>>>>>>> cookie created>>>>", cookies().get("next-auth.session-token")); 
+          // console.log(">>>>>>>>> cookie created>>>>", cookies().get("next-auth.session-token"));
         } else if (isGetCallback) {
           // TODO: only do it for anon session
           console.log("sign in none credential >>>>>>session>>")
-          const currentSessionToken = cookies().get("next-auth.session-token").value;
+          const currentSessionToken = headers.cookies().get("next-auth.session-token")?.value;
           console.log("delete session >>>>>>>>", currentSessionToken);
           if (currentSessionToken) {
-            cookies().delete("next-auth.session-token");
+            headers.cookies().delete("next-auth.session-token");
             console.log("deleted session cookie >>will deleteSession>>>>>>", currentSessionToken);
-            await adapter.purgeSession(currentSessionToken);
+            adapter.deleteSession && await adapter.deleteSession(currentSessionToken);
             console.log("deleted session >>>>>>>>", currentSessionToken);
-          }          
+          }
         } else {
           console.log(">>>>>>>>>>> github signin user", user);
           console.log(">>>>>>>>>>> github signin isGetCallback", isGetCallback);
-          console.log(">>>>>>>>>>> github signin isGetCallback", cookies());
+          console.log(">>>>>>>>>>> github signin isGetCallback", headers.cookies());
         }
         return true;
       },
       async redirect({url, baseUrl }) {
         logger.info('winston redirect ...' + baseUrl);
         log.info('log4js redirect ...' + `${baseUrl}/integrator/configuration-data`);
-        // return `${baseUrl}/integrator/configuration-data`;
-        return `${baseUrl}/home`;
+        return `${baseUrl}/integrator/configuration-data`;
+        // return `${baseUrl}/home`;
         // return baseUrl;
       },
     },
@@ -130,17 +128,17 @@ export const authOptions = (request: NextRequest, context: RouteHandlerContext):
         //   req.query?.nextauth?.includes("callback") &&
         //   req.query?.nextauth?.includes("credentials") &&
         //   req.method === "POST";
-        console.log(">>>>>>>>>jwt encode req query>", params);  
+        console.log(">>>>>>>>>jwt encode req query>", params);
         if (isCredentialsCallback) {
-          console.log(">>>>>>>>>>> jwt encode >>isCredentialsCallback>>>", isCredentialsCallback);  
-          const cookie = cookies().get("next-auth.session-token");
-          console.log(">>>>>>>>>>> jwt encode cookie", cookie.value);  
+          console.log(">>>>>>>>>>> jwt encode >>isCredentialsCallback>>>", isCredentialsCallback);
+          const cookie = headers.cookies().get("next-auth.session-token");
+          console.log(">>>>>>>>>>> jwt encode cookie", cookie?.value);
           if (cookie) return cookie.value;
           return "";
         }
 
         const jwt = encode(arg);
-        console.log(">>>>>>>>>jwt encoded jwt>", jwt);  
+        console.log(">>>>>>>>>jwt encoded jwt>", jwt);
         return jwt;
       },
       decode: async (arg) => {
@@ -150,17 +148,18 @@ export const authOptions = (request: NextRequest, context: RouteHandlerContext):
         //   req.method === "POST";
         console.log(">>>>>>>>>jwt decode req query>", params);
         if (isCredentialsCallback) {
-          console.log(">>>>>>>>>>> jwt decode >>isCredentialsCallback>>>", isCredentialsCallback);  
+          console.log(">>>>>>>>>>> jwt decode >>isCredentialsCallback>>>", isCredentialsCallback);
           return null;
         }
         const decoded = decode(arg);
-        console.log(">>>>>>>>>jwt encoded decoded>", decoded);  
+        console.log(">>>>>>>>>jwt encoded decoded>", decoded);
         return decoded;
       },
     },
     events: {
       async signIn({user, account, profile}: {user: User, account: Account | null, profile?: Profile}): Promise<void> {
-          log.debug(`log4js  signIn of ${user?.name} from ${user?.provider || account?.provider}`);
+          // log.debug(`log4js  signIn of ${user?.name} from ${user?.provider || account?.provider}`);
+          log.debug(`log4js  signIn of ${user?.name}`);
       },
       async signOut({session, token}: {session: Session, token: JWT}): Promise<void> {
           log.debug(`log4js  signOut of ${token?.name} from ${token?.provider}`);
@@ -170,7 +169,7 @@ export const authOptions = (request: NextRequest, context: RouteHandlerContext):
           };
           console.log("sign out >>>>>>>>", sessionToken);
           if (sessionToken) {
-            await adapter.deleteSession(sessionToken);
+            adapter.deleteSession && await adapter.deleteSession(sessionToken);
           }
       },
     },
@@ -179,8 +178,8 @@ export const authOptions = (request: NextRequest, context: RouteHandlerContext):
     pages: {
       signIn: "/auth/signin",
     },
-    debug: process.env.NODE_ENV === "development",
-    secret: process.env.NEXTAUTH_SECRET,
+    debug: process.env['NODE_ENV'] === "development",
+    secret: process.env['NEXTAUTH_SECRET'],
   }
 };
 
@@ -198,7 +197,7 @@ export {handler as GET, handler as POST}
 
 // Helper functions
 
-const createAnonymousUser = async (request: NextRequest, adapter: Adapter): User => {
+const createAnonymousUser = async (request: NextRequest, adapter: Adapter): Promise<User> => {
     console.log(">>>>>>>>>createAnonymousUser>>>");
     const unique_uuid: string = randomUUID();
     const unique_realname = `camel_anon_${unique_uuid}`;
@@ -208,8 +207,7 @@ const createAnonymousUser = async (request: NextRequest, adapter: Adapter): User
         email: `${unique_realname.toLowerCase()}@example.com`,
         name: unique_realname,
         image: "",
-        provider: "anonymous"
+        // provider: "anonymous"
     };
-    
     return user;
 };
