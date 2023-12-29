bd6fe9e867f1c12042a759d3ca9ef1f10d8ee4b4
Frank Li <frank.x.li@gmail.com>
2023-12-29 13:33:26 -0500

    Update page.tsx

--- nextjs-admin-with-session-store/src/app/(apps layout)/(pages)/error-404/page.tsx
+++ nextjs-admin-with-session-store/src/app/(apps layout)/(pages)/error-404/page.tsx
@@ -6,7 +6,7 @@ const PagNotFoundError = () => {
             <h1 className="text-60">404</h1>
             <p className="text-36 subheading mb-3">Error!</p>
             <p className="mb-5  text-muted text-18">
-                The page you were looking for doesn't exist.
+                {`The page you were looking for doesn't exist.`}
             </p>
             <Link href={`/`} className="btn btn-lg btn-primary btn-rounded">
                 Go back to home
