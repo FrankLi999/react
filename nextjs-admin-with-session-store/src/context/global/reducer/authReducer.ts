62699fe889b146bc6f90f2cf3011e3bffe61561b
Frank Li <frank.x.li@gmail.com>
2023-12-29 14:19:56 -0500

    Update authReducer.ts

--- nextjs-admin-with-session-store/src/context/global/reducer/authReducer.ts
+++ nextjs-admin-with-session-store/src/context/global/reducer/authReducer.ts
@@ -1,9 +1,15 @@
 
-export const authInitilalStates = {
+export interface AuthState {
+    authenticated: boolean;
+}
+export interface AuthStateReducerAction {
+    type: string
+}
+export const authInitilalStates: AuthState = {
     authenticated: true
 }
 
-const authReducer = (state = authInitilalStates, action) => {
+const authReducer = (state: AuthState = authInitilalStates, action: AuthStateReducerAction): AuthState => {
     switch (action.type) {
         case "login": {
             return {
