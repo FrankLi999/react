5e70cb7393df87de16a0c4ef3aa8ae2c995fdaad
Frank Li <frank.x.li@gmail.com>
2023-12-29 13:57:46 -0500

    Add files via upload

---
+++ nextjs-admin-with-session-store/src/lib/dataGuard.ts
@@ -0,0 +1,377 @@
+import { deepClone, hasCircularReference, isNullish, isObject, isString } from './helpers';
+
+export type SensitiveContentKey = keyof typeof sensitiveContentRegExp;
+type MaskingChar = 'X' | 'x' | '$' | '/' | '.' | '*' | '#' | '+' | '@' | '-' | '_' | ' ';
+
+export interface IMaskDataOptions {
+    keyCheck: (key: string) => boolean;
+    immutable: boolean;
+    customPatterns: Record<string, RegExp>;
+    maskingChar: MaskingChar;
+    maskLength: number;
+    types: SensitiveContentKey[];
+    customSensitiveContentRegExp: Record<string, RegExp>;
+    /**
+     * When enabled, masks the sensitive content with a fixed number of characters,
+     * irrespective of the original content's length. This prevents inferring the
+     * length of the original content from the masked output.
+     * Default is 'false', which means the mask will reflect the original content's length.
+     */
+    fixedMaskLength: boolean;
+    excludeMatchers?: SensitiveContentKey[];
+}
+
+// const defaultSensitiveKeyFragments: Set<string> = new Set([
+const defaultSensitiveKeyFragments = [    
+    'password',
+    'pwd',
+    'pass',
+    'token',
+    'secret',
+    'key',
+    'passphrase',
+    'privateKey',
+    'mail',
+    'shared',
+    'credential',
+    'auth',
+    'signature',
+    'certificate',
+    'crypt',
+    'apikey',
+    'security',
+    'phone',
+    'mobile'
+];
+
+const sensitiveContentRegExp = {
+    uuid: /\b[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\b/gi,
+    creditCard: /\b(?:\d[ -]*?){13,16}\b/g,
+    ssn: /\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b/g,
+    url: /\b(?:https?|ftp):\/\/[a-z0-9-+&@#/%?=~_|!:,.;]*[a-z0-9-+&@#/%=~_|]\b/gi,
+    ipv4: /\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/g,
+    email: /(?<=^|[\s'"-#+.><])[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g,
+    passwordSubstring: /\b(?!password\b)\w*password\w*\b/gi,
+    password: /\b(?!\w*\/)(?=\S*\d)(?=\S*[A-Za-z])[\w!@#$%^&*()_+=\-,.]{6,32}\b/gm,
+    passwordInUri: /(?<=:\/\/[^:]+:)[^@]+?(?=@)/,
+    passwordMention: /(?<=.*(password|passwd|pwd)(?:\s*:\s*|\s+))\S+/gi
+} as const;
+
+function maskSensitiveValue(value: string, options: Partial<IMaskDataOptions>): string {
+    const skipMaskingPattern = /##([^#]*)##/g; // Pattern to match content that should not be masked
+    const maskLength = options?.maskLength || value.length - 4;
+    const maskingChar = options?.maskingChar || '*';
+    // Skip masking if the entire value is wrapped in '##'
+    if (skipMaskingPattern.test(value)) {
+        return value;
+    }
+
+    if (options?.fixedMaskLength) {
+        return maskingChar.repeat(16);
+    }
+
+    if (value.length <= 4 || maskLength >= value.length) {
+        return maskingChar.repeat(value.length);
+    }
+    const visibleLength = (value.length - maskLength) / 2;
+    return (
+        value.substring(0, visibleLength) +
+        maskingChar.repeat(maskLength) +
+        value.substring(value.length - visibleLength)
+    );
+}
+
+function shouldMaskKey(key: string): boolean {
+    const lowerCaseKey = key.toLowerCase();
+    for (const sensitiveKey of defaultSensitiveKeyFragments) {
+        if (lowerCaseKey.includes(sensitiveKey)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function maskSensitiveContent(
+    originalValue: string,
+    defaultPatterns: typeof sensitiveContentRegExp,
+    customPatterns: Record<string, RegExp> = {},
+    options?: Partial<IMaskDataOptions>
+): string {
+    const allPatterns = { ...defaultPatterns, ...customPatterns };
+    const skipMaskingPattern = /##([^#]*)##/g; // Pattern to match content that should not be masked
+
+    const applyPatternMasking = (currentValue: string, pattern: RegExp): string => {
+        let result = '';
+        let lastIndex = 0;
+
+        // Process parts of the string outside '##' blocks
+        currentValue.replace(skipMaskingPattern, (match, p1, offset) => {
+            // Apply masking to the content before the '##' block
+            const substring = currentValue.substring(lastIndex, offset);
+            result += substring.replace(pattern, match => maskSensitiveValue(match, options as Partial<IMaskDataOptions>));
+            // Don't mask inside the '##' block
+            result += match;
+            lastIndex = offset + match.length;
+            return match; // Necessary for the replace function, though it's not used here
+        });
+
+        // Apply masking to the content after the last '##' block
+        const substring = currentValue.substring(lastIndex);
+        result += substring.replace(pattern, match => maskSensitiveValue(match, options as Partial<IMaskDataOptions>));
+
+        pattern.lastIndex = 0; // Reset regex state for global patterns
+        return result;
+    };
+
+    return Object.values(allPatterns).reduce(
+        (maskedValue, pattern) => applyPatternMasking(maskedValue, pattern),
+        originalValue
+    );
+}
+
+function unmaskContent(value: string): { isUnmasked: boolean; content: string } {
+    const unmaskPattern = /##(.*?)##/g;
+    let isUnmasked = false;
+    const content = value.replace(unmaskPattern, (match, capture) => {
+        isUnmasked = true;
+        return capture;
+    });
+
+    return { isUnmasked, content };
+}
+
+/**
+ * Masks sensitive parts of a string based on predefined and custom patterns.
+ *
+ * @param {string} value - The string to mask.
+ * @returns {string} The masked string.
+ */
+export function maskString(value: string): string;
+
+/**
+ * Masks sensitive parts of a string based on provided options.
+ *
+ * @param {string} value - The string to mask.
+ * @param {Partial<IMaskDataOptions>} options - The options to customize the masking process.
+ * @returns {string} The masked string.
+ */
+export function maskString(value: string, options: Partial<IMaskDataOptions>): string;
+
+/**
+ * Masks sensitive parts of a string based on specified types and optionally custom patterns and options.
+ *
+ * @param {string} value - The string to mask.
+ * @param {SensitiveContentKey[]} types - The types of sensitive content to mask.
+ * @param {Record<string, RegExp>} [customSensitiveContentRegExp] - Custom regular expressions for detecting sensitive content.
+ * @param {Partial<IMaskDataOptions>} [options] - The options to customize the masking process.
+ * @returns {string} The masked string.
+ */
+export function maskString(
+    value: string,
+    types: SensitiveContentKey[]|undefined, // This remains non-optional
+    customSensitiveContentRegExp?: Record<string, RegExp>,
+    options?: Partial<IMaskDataOptions>
+): string;
+
+export function maskString(
+    value: string,
+    typesOrOptions?: SensitiveContentKey[] | Partial<IMaskDataOptions>, // This should be optional
+    customSensitiveContentRegExp: Record<string, RegExp> = {},
+    options?: Partial<IMaskDataOptions>
+): string {
+    if (!value || value.length === 0) return value;
+
+    let types: SensitiveContentKey[] | undefined;
+    if (typesOrOptions && !Array.isArray(typesOrOptions)) {
+        // If typesOrOptions is not an array, it means it's the options parameter
+        options = typesOrOptions;
+    } else {
+        types = typesOrOptions as SensitiveContentKey[];
+    }
+
+    if (!value || value.length === 0) return value;
+
+    // Check if the content is marked to be unmasked
+    const { isUnmasked, content } = unmaskContent(value);
+    if (isUnmasked) {
+        return content; // return content without '##' and without masking
+    }
+
+    if (!types) types = Object.keys(sensitiveContentRegExp) as SensitiveContentKey[];
+    if (!customSensitiveContentRegExp) customSensitiveContentRegExp = {};
+    if (options?.excludeMatchers) {
+        // types = types.filter(t => !options?.excludeMatchers.includes(t));
+    }
+
+    const applicablePatterns = types.reduce(
+        (acc, type) => {
+            if (type in sensitiveContentRegExp) {
+                // check if 'type' is a valid key to satisfy TypeScript's safety checks
+                acc[type] = sensitiveContentRegExp[type]; // TypeScript now knows 'type' is a valid key
+            }
+            return acc;
+        },
+        {} as Record<SensitiveContentKey, RegExp>
+    ); // Also define the accumulator's type
+
+    return maskSensitiveContent(value, applicablePatterns, customSensitiveContentRegExp, options);
+}
+
+function maskMap<K, V>(item: Map<K, V>, options: Partial<IMaskDataOptions>): Map<K, V> {
+    // Process Map items
+    const processedMap = options.immutable ? new Map<K, V>() : item;
+
+    item.forEach((value, key) => {
+        if (typeof key === 'string') {
+            processedMap.set(key, performMasking(key, value, options));
+        } else {
+            // If the key is not a string, we can't check it against sensitive content, but we still mask the value.
+            processedMap.set(key, maskData(value, options));
+        }
+    });
+
+    return processedMap;
+}
+
+function maskSet<T>(item: Set<T>, options: Partial<IMaskDataOptions>): Set<T> {
+    const processedSet = options.immutable ? new Set<T>() : item;
+
+    const toAdd: T[] = [];
+    const toRemove: T[] = [];
+
+    item.forEach(value => {
+        let maskedValue;
+        if (typeof value === 'string') {
+            // If the value is a string, perform masking
+            maskedValue = maskString(value, undefined, options.customPatterns, options) as T;
+        } else if (isObject(value) || Array.isArray(value) || value instanceof Map) {
+            // If the value is an object, array, or map, recursively call maskData
+            maskedValue = maskData(value, options);
+        } else {
+            // If the value is of any other type, just keep the original value
+            maskedValue = value as T;
+        }
+
+        if (options.immutable) {
+            // If immutability is required, add masked value to the new set
+            processedSet.add(maskedValue as T);
+        } else {
+            // If immutability is not required, track the original and masked values for later update
+            toAdd.push(maskedValue as T);
+            toRemove.push(value as T);
+        }
+    });
+
+    if (!options.immutable) {
+        // Update the original set
+        toRemove.forEach(value => item.delete(value));
+        toAdd.forEach(value => item.add(value));
+    }
+
+    return processedSet;
+}
+
+function maskError(item: Error, options: Partial<IMaskDataOptions>): Error {
+    const maskedError = new Error(maskString(item.message, undefined, options.customPatterns, options));
+    maskedError.stack = maskString(item.stack as string, undefined, options.customPatterns, options);
+    return maskedError;
+}
+
+function maskObject<T>(item: T, options: Partial<IMaskDataOptions>): T {
+    if (hasCircularReference(item)) {
+        return item;
+    }
+    // Clone the item if immutability is required
+    const processedItem = options.immutable ? deepClone(item) : item;
+
+    const assignMaskedValue = (obj: Record<string, unknown>, key: string, value: unknown) => {
+        obj[key] = performMasking(key, value, options);
+    };
+
+    return Object.entries(processedItem as Object).reduce(
+        (acc, [key, value]) => {
+            assignMaskedValue(acc as Record<string, T>, key, value);
+            return acc;
+        },
+        options.immutable ? ({} as Record<string, unknown>) : processedItem
+    ) as T;
+}
+
+function maskArray<T>(item: T[], options: Partial<IMaskDataOptions>): T[] {
+    return item.map(i => maskData(i, options));
+}
+
+function performMasking<T>(key: string, value: unknown, options: Partial<IMaskDataOptions>): T {
+    // Check if the content is marked to be unmasked and it's a string
+    if (isString(value)) {
+        const { isUnmasked, content } = unmaskContent(value as string);
+        if (isUnmasked) {
+            return content as T; // return content without '##' and without masking
+        }
+    }
+    return (
+        options.keyCheck && options.keyCheck(key)
+            ? isString(value)
+                ? maskSensitiveValue(value as string, options) // Pass maskLength here
+                : maskData(value, options)
+            : maskData(value, options)
+    ) as T;
+}
+
+/**
+ * Masks data based on the type and the provided masking options.
+ * This function is recursively called for nested objects.
+ *
+ * @template T
+ * @param {T} item - The original data to mask.
+ * @param {Partial<IMaskDataOptions>} [options={}] - Optional masking options.
+ * @returns {T} The masked data.
+ */
+export function maskData<T>(item: T, options: Partial<IMaskDataOptions> = {}): T {
+    const { keyCheck = shouldMaskKey, immutable = true } = options;
+
+    if (isString(item)) {
+        // Pass custom regex patterns to maskString
+        return maskString(item as string, undefined, options.customPatterns, options) as T;
+    }
+
+    options.keyCheck = keyCheck;
+    options.immutable = immutable;
+
+    if (item instanceof Map) {
+        return maskMap(item, options) as T;
+    }
+
+    if (item instanceof Set) {
+        return maskSet(item, options) as T;
+    }
+
+    if (item instanceof Error) {
+        return maskError(item, options) as T;
+    }
+
+    if (isObject(item)) {
+        return maskObject<T>(item, options);
+    }
+
+    if (Array.isArray(item)) {
+        return maskArray(item, options) as T;
+    }
+
+    return item as T;
+}
+
+/**
+ * Masks arguments of a function call, based on the provided masking options.
+ *
+ * @template T
+ * @param {unknown[]} args - The original arguments to mask.
+ * @param {IMaskDataOptions} [options] - Optional masking options.
+ * @returns {T[]} The masked arguments.
+ */
+export function maskArguments<T>(args: unknown[], options?: IMaskDataOptions): T[] {
+    if (isNullish(args) || args.length === 0) return args as T[];
+    return args.map(arg =>
+        typeof arg !== 'object' && typeof arg !== 'string' ? arg : maskData<T>(arg as T, options)
+    ) as T[];
+}
---
+++ nextjs-admin-with-session-store/src/lib/helpers.ts
@@ -0,0 +1,92 @@
+export function hasCircularReference<T>(item: T): boolean {
+    const seenObjects = new WeakSet<object>();
+
+    function detect(obj: unknown): boolean {
+        if (typeof obj !== 'object' || obj === null) {
+            return false;
+        }
+
+        if (seenObjects.has(obj as object)) {
+            return true;
+        }
+
+        seenObjects.add(obj as object);
+
+        for (const key in obj as Record<PropertyKey, unknown>) {
+            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                if (detect((obj as Record<PropertyKey, unknown>)[key])) {
+                    return true;
+                }
+            }
+        }
+
+        // No need to delete the object from seenObjects since it's now a WeakSet
+
+        return false;
+    }
+
+    return detect(item);
+}
+
+export function deepClone<T>(obj: T, visited = new WeakMap()): T {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
+    // Check for circular references
+    if (visited.has(obj)) {
+        return visited.get(obj) as T;
+    }
+
+    if (obj instanceof Date) {
+        return new Date(obj.getTime()) as unknown as T;
+    }
+
+    if (Array.isArray(obj)) {
+        const arrCopy: T[] = [];
+        // Store reference to new array
+        visited.set(obj, arrCopy);
+        arrCopy.push(...obj.map(item => deepClone(item, visited)));
+        return arrCopy as T;
+    }
+
+    if (obj instanceof Object) {
+        // Get the prototype of the object
+        const proto = Object.getPrototypeOf(obj);
+
+        // Create a new object with the same prototype
+        const objCopy: Record<string, unknown> = Object.create(proto);
+
+        // Store reference to new object
+        visited.set(obj, objCopy);
+
+        // Iterate over all keys, including inherited ones
+        const allKeys = [...Object.getOwnPropertyNames(obj), ...Object.getOwnPropertySymbols(obj)];
+        allKeys.forEach(key => {
+            const descriptor = Object.getOwnPropertyDescriptor(obj, key);
+            if (descriptor && typeof descriptor.value === 'object' && descriptor.value !== null) {
+                // If the value is an object, deep clone it
+                descriptor.value = deepClone(descriptor.value, visited);
+            }
+            // Define the property on the new object
+            Object.defineProperty(objCopy, key, descriptor as PropertyDescriptor);
+        });
+
+        return objCopy as T;
+    }
+    //TODO
+    return obj;
+}
+// item is Record<string, unknown>
+export function isObject(item: unknown): boolean  {
+    return (!isNullish(item)) && typeof item === 'object' && !Array.isArray(item) as boolean;
+}
+
+//  value is string
+export function isString(value: unknown): boolean {
+    return typeof value === 'string';
+}
+
+export function isNullish(value: unknown): boolean {
+    return value === undefined || value === null;
+}
