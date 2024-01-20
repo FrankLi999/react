b6a904cc226ae86f37473886b176ac9f26259fc7
Frank Li <frank.x.li@gmail.com>
2024-01-20 17:02:27 -0500

    Added spring boot config server

---
+++ spring-boot-config-jdbc/src/main/java/com/example/springbootconfigjdbc/controller/DashboardConfig.java
@@ -0,0 +1,26 @@
+package com.example.springbootconfigjdbc.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DashboardConfig implements WebMvcConfigurer {
+    @Override
+   public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+       // @formatter:off
+       // S2i Integrator React static resources
+       System.out.println(">>>>>>>>>>>>addResourceHandlers >>>>>>>>>>>>");
+       registry
+               .addResourceHandler("/camel/static/**")
+               .addResourceLocations("classpath:/my-camel-static/static/");
+       registry
+               .addResourceHandler("/camel/static/img/**")
+               .addResourceLocations("classpath:/my-camel-static/img/");
+       registry
+               .addResourceHandler("/camel/*")
+               .addResourceLocations("classpath:/my-camel-static/");
+
+       // @formatter:on
+   }
+}
---
+++ spring-boot-config-jdbc/src/test/java/com/example/springbootconfigjdbc/SpringBootConfigJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootconfigjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
