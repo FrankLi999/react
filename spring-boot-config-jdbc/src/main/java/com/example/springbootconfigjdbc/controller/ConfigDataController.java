0b9b71ee5cdf08757c20d4b446d1de771e56a44d
Frank Li <frank.x.li@gmail.com>
2024-03-17 11:43:30 -0400

    Update ConfigDataController.java

--- spring-boot-config-jdbc/src/main/java/com/example/springbootconfigjdbc/controller/ConfigDataController.java
+++ spring-boot-config-jdbc/src/main/java/com/example/springbootconfigjdbc/controller/ConfigDataController.java
@@ -15,12 +15,18 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/spring/admin")
+@RequestMapping("/spring/admin/api")
 public class ConfigDataController {
    @Autowired
    private ConfigDataService configDataService;
@@ -32,34 +38,72 @@ public class ConfigDataController {
        this.objectMapper = objectMapper;
    }
 
-   @GetMapping(path="api/configurations", produces = MediaType.APPLICATION_JSON_VALUE)
+   @GetMapping(path="configurations", produces = MediaType.APPLICATION_JSON_VALUE)
    @ResponseStatus(HttpStatus.OK)
    public List<ConfigData> findAll() {
        return configDataService.findAll();
    }
 
-   @PostMapping(path="api/configurations", consumes = MediaType.APPLICATION_JSON_VALUE)
+   @PostMapping(path="configurations", consumes = MediaType.APPLICATION_JSON_VALUE)
    @ResponseStatus(HttpStatus.CREATED)
    public void post(@RequestBody List<ConfigData> configData) {
        configDataService.createAll(configData);
    }
 
-   @PostMapping(path = "api/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+   @PostMapping(path = "import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
    @ResponseStatus(HttpStatus.OK)
    public List<ConfigData> upload(@RequestParam("file") MultipartFile file) throws IOException, JsonProcessingException {
        List<ConfigData> configData = objectMapper.readValue(file.getInputStream(), new TypeReference<List<ConfigData>>(){});
        return configDataService.recreateAllAndFindAll(configData);
    }
 
-   @PutMapping(path="api/configurations", consumes = MediaType.APPLICATION_JSON_VALUE)
+   @PostMapping(path = "sql", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<ConfigData> losqSql(@RequestParam("file") MultipartFile file) throws IOException, SQLException {
+        String sql = new String(file.getBytes());
+        return configDataService.loadSql(sql);
+    }
+   
+   @PutMapping(path="configurations", consumes = MediaType.APPLICATION_JSON_VALUE)
    @ResponseStatus(HttpStatus.CREATED)
    public void put(@RequestBody List<ConfigData> configData) {
        configDataService.recreateAll(configData);
    }
 
-   @DeleteMapping(path="api/configurations", consumes = MediaType.APPLICATION_JSON_VALUE)
+   @DeleteMapping(path="configurations", consumes = MediaType.APPLICATION_JSON_VALUE)
    @ResponseStatus(HttpStatus.OK)
    public List<ConfigData> deleteApplicationProfiles(@RequestBody List<ApplicationProfile> applicationProfiles) {
        return configDataService.deleteApplicationProfiles(applicationProfiles);
    }
+
+       /**
+     *
+     * @param token
+     * @return response like,     *
+     * {
+     *   "token": "d-lp0ZL5WLrtOMzo7_xzBnEkpHh8aL6uW0EBjPcyaHDjSasETttc5fPBYI7ADaqJjtFHNhQViRpLXo-Da3gw6sFTDhHVeZlh",
+     *   "parameterName": "_csrf",
+     *   "headerName": "X-CSRF-TOKEN"
+     * }
+     *
+     *
+     * import axios from 'axios';
+     *
+     * function post(path, data) {
+     *     return axios.get("/csrf")
+     *             .then(tokenResp => {
+     *                 let config = {
+     *                     headers: {
+     *                         'X-CSRF-TOKEN': tokenResp.data.token,
+     *                     }
+     *                   }
+     *                 return axios.post(path, data, config);
+     *             })
+     *             .then(res => res.data)
+     * }
+     */
+    @PostMapping("csrf")
+    public CsrfToken csrf(CsrfToken token) {
+        return token;
+    }
 }
