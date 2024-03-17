8beb9dc9716b73552ae554c9514a46149350dc46
Frank Li <frank.x.li@gmail.com>
2024-03-17 11:41:01 -0400

    Update FilerConfig.java

--- spring-boot-config-jdbc/src/main/java/com/example/springbootconfigjdbc/config/FilerConfig.java
+++ spring-boot-config-jdbc/src/main/java/com/example/springbootconfigjdbc/config/FilerConfig.java
@@ -6,16 +6,16 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.springbootconfigjdbc.filter.FrontendForwardingFilter;
 
-@Configuration
+// @Configuration
 public class FilerConfig {
-    @Bean
-    public FilterRegistrationBean<FrontendForwardingFilter> filters() {
-        FilterRegistrationBean<FrontendForwardingFilter> registrationBean = new FilterRegistrationBean<>();
+    // @Bean
+    // public FilterRegistrationBean<FrontendForwardingFilter> filters() {
+    //     FilterRegistrationBean<FrontendForwardingFilter> registrationBean = new FilterRegistrationBean<>();
 
-        registrationBean.setFilter(new FrontendForwardingFilter());
-        // /s2i-integrator/config/spring/admin/api/configurations
-        registrationBean.addUrlPatterns("/spring/admin/*");
-        registrationBean.setOrder(999);
-        return registrationBean;
-    }
+    //     registrationBean.setFilter(new FrontendForwardingFilter());
+    //     // /s2i-integrator/config/spring/admin/api/configurations
+    //     registrationBean.addUrlPatterns("/spring/admin/*");
+    //     registrationBean.setOrder(999);
+    //     return registrationBean;
+    // }
 }
