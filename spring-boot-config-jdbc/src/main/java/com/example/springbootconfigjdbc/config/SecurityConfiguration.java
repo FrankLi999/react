a31d11e6687b4683d68e678006bc5d78164e29f1
Frank Li <frank.x.li@gmail.com>
2024-03-21 14:16:02 -0400

    Update SecurityConfiguration.java

--- spring-boot-config-jdbc/src/main/java/com/example/springbootconfigjdbc/config/SecurityConfiguration.java
+++ spring-boot-config-jdbc/src/main/java/com/example/springbootconfigjdbc/config/SecurityConfiguration.java
@@ -31,42 +31,50 @@ public class SecurityConfiguration {
  
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthorizationProperties props) throws Exception {
-        http.csrf(csrf -> csrf.ignoringRequestMatchers("/spring/admin/api/encrypt/*", "/spring/admin/api/decrypt/*", "/spring/admin/api/csrf")
-                    // XSRF-TOKEN cookie won’t be marked HTTP-only, so React can read it and send it back when it tries to manipulate data
-                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                    // https://stackoverflow.com/questions/74447118/csrf-protection-not-working-with-spring-security-6/74521360#74521360
-                    // The default behavior of Spring Security 6 is to postpone looking up the CsrfToken until it is required.
-                    // add this if it needs the token every time
+       
+        // http.csrf().disable();
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/spring/admin/api/csrf")
+                // XSRF-TOKEN cookie won’t be marked HTTP-only, so React can read it and send it back when it tries to manipulate data
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                // https://stackoverflow.com/questions/74447118/csrf-protection-not-working-with-spring-security-6/74521360#74521360
+                // The default behavior of Spring Security 6 is to postpone looking up the CsrfToken until it is required.
+                // add this if it needs the token every time
 
-                    // https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#csrf-integration-javascript-spa-configuration
-                    .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
-                ). 
-          authorizeHttpRequests((authz -> authz.requestMatchers("/actuator/**").permitAll()
-                    // .requestMatchers("/ui/home/mmm/nnn/ooo/ppp/**").permitAll()
-                    // .requestMatchers("/admin/console/properties/index.html").permitAll()
-                    .requestMatchers("/spring/admin/dashboard", "/spring/admin/dashboard/index.html", "/spring/admin/dashboard/static/**",
-                            "/spring/admin/dashboard/*.ico", "/spring/admin/dashboard/*.json", "/spring/admin/dashboard/*.png").permitAll()
-                    .requestMatchers("/spring/admin/api/**").permitAll()
-                    .anyRequest().authenticated()))
-                 // Configure a custom Filter to load the CsrfToken on every request, which will return a new cookie if needed.
-                 .addFilterAfter(new CookieCsrfFilter(), BasicAuthenticationFilter.class)
-                 .addFilterAfter(new SpaWebFilter(), BasicAuthenticationFilter.class);
-     
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
+                // https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#csrf-integration-javascript-spa-configuration
+                .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
+            ). //.ignoringRequestMatchers("/encrypt/**").ignoringRequestMatchers("/decrypt/**")
+             authorizeHttpRequests((authz -> authz.requestMatchers("/actuator/**").permitAll()
+                // .requestMatchers("/ui/home/mmm/nnn/ooo/ppp/**").permitAll()
+                // .requestMatchers("/admin/console/properties/index.html").permitAll()
+                .requestMatchers("/spring/admin/dashboard", "/spring/admin/dashboard/index.html", "/spring/admin/dashboard/static/**",
+                        "/spring/admin/dashboard/*.ico", "/spring/admin/dashboard/*.json", "/spring/admin/dashboard/*.png").permitAll()
+                .requestMatchers("/spring/admin/api/**").permitAll()
+                .anyRequest().authenticated()))
+             // Configure a custom Filter to load the CsrfToken on every request, which will return a new cookie if needed.
+             .addFilterAfter(new CookieCsrfFilter(), BasicAuthenticationFilter.class)
+             .addFilterAfter(new SpaWebFilter(), BasicAuthenticationFilter.class);
+        
+        http.formLogin();
+        http.httpBasic();
         http.oauth2Login(oauth2 -> oauth2.userInfoEndpoint(ep ->
-                        ep.oidcUserService(customOidcUserService(props))));
+                ep.oidcUserService(customOidcUserService(props))));
         return http.build();
     }
 
+
     @Bean
     public RequestCache refererRequestCache() {
-        return (HttpSessionRequestCache) saveRequest(request, response) -> {
-            String referer = request.getHeader("referer");
-            if (referer == null) {
-                referer = request.getRequestURL().toString();
+        return new HttpSessionRequestCache() {
+            @Override
+            public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+                String referrer = request.getHeader("referer");
+                if (referrer == null) {
+                    referrer = request.getRequestURL().toString();
+                }
+                request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST",
+                        new SimpleSavedRequest(referrer));
+
             }
-            request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST", new SimpleSavedRequest(referer));
         };
     }
 
